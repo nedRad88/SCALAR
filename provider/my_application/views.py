75c5a24dfcc6372e5000cd65c115ed84bf822c66
nedRad88 <nedeljko.radulovic88@gmail.com>
2019-07-28 21:57:17 +0200

    changed dns_name in app.js

--- provider/my_application/views.py
+++ provider/my_application/views.py
@@ -26,7 +26,7 @@ from repository import MongoRepository
 from flask import send_file
 import logging
 
-logging.basicConfig(level='DEBUG')
+logging.basicConfig(level='INFO')
 from itsdangerous import URLSafeTimedSerializer
 import csv
 from io import BytesIO
@@ -60,7 +60,7 @@ app.config['MAIL_USE_SSL'] = True
 app.config['SECRET_KEY'] = 's3cr3t'
 app.config['SECURITY_PASSWORD_SALT'] = 'my_precious_two'
 mail = Mail(app)
-app.debug = True
+app.debug = False
 
 _SCHEDULER = Scheduler()
 _SCHEDULER.start()
