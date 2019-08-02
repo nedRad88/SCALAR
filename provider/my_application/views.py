e0cf87a1e16ae7c51190bb5a86745b5deaac53e6
nedRad88 <nedeljko.radulovic88@gmail.com>
2019-08-03 00:08:07 +0200

    changes in views

--- provider/my_application/views.py
+++ provider/my_application/views.py
@@ -795,6 +795,6 @@ def download_proto_file(competition_id):
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port='5000', debug=True)
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
-    # wsgi.server(eventlet.listen(('', 5000)), app)
+    # http_server = WSGIServer(('', 5000), app)
+    # http_server.serve_forever()
+    wsgi.server(eventlet.listen(('', 5000)), app)
