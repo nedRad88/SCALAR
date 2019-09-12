76a717055a53858653803faa1821809e787ae49d
nedRad88 <nedeljko.radulovic88@gmail.com>
2019-09-12 23:41:53 +0200

    for merge

--- provider/test2.py
+++ provider/test2.py
@@ -1,13 +1,11 @@
-import datetime
-import json
-from bson import json_util
 from werkzeug.security import generate_password_hash, check_password_hash
 
-password = "5eIWqOotMopq"
+
+password = 'sMECgJ87HhGH'
 
 hashed = generate_password_hash(password)
 
-
 print(hashed)
 
-#print(check_password_hash(hashed_old, password))
+
+
